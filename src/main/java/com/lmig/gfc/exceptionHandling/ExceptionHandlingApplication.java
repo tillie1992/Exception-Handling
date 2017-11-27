8cce302e2763019c405d5365c4e442fad14c4640
Marci Taylor <Marci.taylor@libertymutual.com>
2017-11-27 15:53:49 -0500

    Exception handling

---
+++ src/main/java/com/lmig/gfc/exceptionHandling/ExceptionHandlingApplication.java
@@ -0,0 +1,12 @@
+package com.lmig.gfc.exceptionHandling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptionHandlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExceptionHandlingApplication.class, args);
+	}
+}
